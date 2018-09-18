93daaee3663d63bed31568fb532a761f98fff8bf
mightyfrog <mightyfrog.gc@gmail.com>
2018-09-19 03:29:22 +0900

    rewrite with CoroutineScope

--- app/src/main/java/org/mightyfrog/android/kotlincoroutinerepeatsample/MainActivity.kt
+++ app/src/main/java/org/mightyfrog/android/kotlincoroutinerepeatsample/MainActivity.kt
@@ -1,18 +1,19 @@
 package org.mightyfrog.android.kotlincoroutinerepeatsample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.android.Main
+import kotlin.coroutines.experimental.CoroutineContext
 
 /**
  * @author Shigehiro Soejima
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private val job = Job()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,19 +21,21 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { _ ->
-            launch(UI) {
+            launch(Dispatchers.Main) {
                 repeat(10) {
-                    withContext(DefaultDispatcher) {
+                    withContext(Dispatchers.Default) {
                         Thread.sleep(1000L)
                     }
-                    textView.text = java.util.Date(System.currentTimeMillis()).toString()
+                    val time = java.util.Date(System.currentTimeMillis()).toString()
+                    Log.d("coroutine", time)
+                    textView.text = time
                     // return@launch to exit prematurely
                     return@repeat
                 }
             }
 
-            // as retry
-//            launch(UI) {
+//             as retry
+//            launch(Dispatchers.Main) {
 //                repeat(10) {
 //                    try {
 //                        // do something
@@ -44,4 +47,12 @@ class MainActivity : AppCompatActivity() {
 //            }
         }
     }
+
+    override fun onDestroy() {
+        job.cancel()
+
+        super.onDestroy()
+    }
+
+    override val coroutineContext: CoroutineContext get() = Dispatchers.Main + job
 }
